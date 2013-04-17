0565d16578fe1c23a2e594c1cda95ea2b266d52b
Mina Nagy Zaki <mnzaki@gmail.com>
2013-04-17 09:30:20 +0200

    Add utility methods to Hackathon
    Storage paths and currently active singleton hackathon

--- se-hackathon/app/models/hackathon.rb
+++ se-hackathon/app/models/hackathon.rb
@@ -3,4 +3,25 @@ class Hackathon < ActiveRecord::Base
 
   has_and_belongs_to_many :hackers
   has_many :user_stories
+
+  HACKATHONS_ROOT = "#{::Rails.root}/hackathons"
+
+  def self.active
+    unless @@active and @@active.active?
+      @@active = Hackathon.where(active: true).first
+    end
+    @@active
+  end
+
+  def storage_path
+    "#{HACKATHONS_ROOT}/#{id}"
+  end
+
+  def git
+    @git ||= Rugged::Repository.new("#{storage_path}/repo")
+  end
+
+  def tests_path
+    "#{storage_path}/tests"
+  end
 end
